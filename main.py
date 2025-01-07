b2a29914e17b9b269022fd4cb78ee9ed8f01cc35
Matej Papac <papac.tmrip@gmail.com>
2025-01-07 14:43:19 +0100

    Model Trainer  Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.projectone import logger
 from src.projectone.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.projectone.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.projectone.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.projectone.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -36,6 +37,18 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = 'Model Trainer stage'
+
+try:
+        logger.info(f'>>>> stage {STAGE_NAME} started <<<<')
+        obj=ModelTrainerPipeline()
+        obj.initiate_model_training()
+        logger.info(f'>>>> stage {STAGE_NAME} completed <<<< \n\nx=======x')
+except Exception as e:
+        logger.exception(e)
+        raise e
+        
 
 
 
